36a0bb564739dffe78fe6277a23aa942c3906b6a
ZA Syed <ali.syed@gauntletai.com>
2025-02-03 15:27:33 -0600

    Initial Commit

---
+++ RipTawk/RipTawkApp.swift
@@ -0,0 +1,17 @@
+//
+//  RipTawkApp.swift
+//  RipTawk
+//
+//  Created by Zahad Ali Syed on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct RipTawkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RipTawkTests/RipTawkTests.swift
@@ -0,0 +1,17 @@
+//
+//  RipTawkTests.swift
+//  RipTawkTests
+//
+//  Created by Zahad Ali Syed on 2/3/25.
+//
+
+import Testing
+@testable import RipTawk
+
+struct RipTawkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
