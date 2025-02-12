7126e61b85f38ce997b3f4135a87eb6c996104b2
ZA Syed <ali.syed@gauntletai.com>
2025-02-12 12:14:06 -0600

    update transcribe func

--- functions/transcribe/src/index.js
+++ functions/transcribe/src/index.js
@@ -82,3 +82,10 @@ export default async function(req, res) {
     res.json({ error: error.toString() });
   }
 }
+
+// Add module.exports for compatibility
+module.exports = {
+  default: async function(req, res) {
+    return await exports.default(req, res);
+  }
+};
